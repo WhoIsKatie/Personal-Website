26f758b7df272a349703c1d6587e2892be1ded5f
Katie Liu <katieliu505@gmail.com>
2022-02-06 20:10:03 -0500

    update navbar

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 
+const navBarToggle = document.getElementById('js-navbar-toggle');
+const sections = document.getElementById('js-menu');
+const navbar = document.getElementById('navbar');
+const navLinks = document.querySelectorAll('li');
+var enable = true;
+
+navBarToggle.addEventListener('click', function () {
+    sections.classList.toggle('active');
+    navbar.classList.toggle('bgactive');
+    enable = !enable;
+});
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => {
+        sections.classList.toggle('active');
+        navbar.classList.toggle('bgactive');
+        enable = !enable;
+    })
+});
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("nav-bar").style.top = "0";
-    } else {
-        document.getElementById("nav-bar").style.top = "-70px";
+    if (enable == true) {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-70px";
+        }
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
 }
