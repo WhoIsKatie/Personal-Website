fc16fd117916bf28cee7bce79e72f6cd2c9c27c9
Katie Liu <katieliu505@gmail.com>
2022-12-30 18:40:55 -0500

    update theme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Personal-Website v1.0
 
-This is the first iteration of my personal website. No matter how rudimentary it seemed to me at the time, I'm truly proud of myself for having built this draft during one of my most harrowing phases of my coming of age. 
+Ta-da!! This is my first personal website!
+No matter how rudimentary it may seem to me, I'm quite proud of myself for building this despite my initial circumstances.
--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-
-const navBarToggle = document.getElementById('js-navbar-toggle');
 const sections = document.getElementById('js-menu');
 const navbar = document.getElementById('navbar');
 const navLinks = document.querySelectorAll('li');
 var enable = true;
 
-navBarToggle.addEventListener('click', function () {
-    sections.classList.toggle('active');
-    navbar.classList.toggle('bgactive');
-    enable = !enable;
-});
-
 navLinks.forEach((l) => {
+    //gradientChars(l);
+
     l.addEventListener('click', () => {
         sections.classList.toggle('active');
         navbar.classList.toggle('bgactive');
-        enable = !enable;
+        //enable = !enable;
     })
 });
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-    if (enable == true) {
+    if (enable) {
         var currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
             document.getElementById("navbar").style.top = "0";
@@ -31,3 +25,68 @@ window.onscroll = function () {
         prevScrollpos = currentScrollPos;
     }
 }
+
+/**
+ * 
+ * @param {*} link 
+ * @returns 
+ */
+/* function gradientChars(link) {
+    if (link == null) return;
+    if (link.querySelector('a') == null) return;
+
+    let current = link.querySelector('a');
+    let text = current.textContent.split('');
+    let hue = window.getComputedStyle(current).getPropertyValue('color');
+    let rgbVals = hue.replace(/ /g, '').slice(4, -1).split(',').map(e => parseInt(e));
+    const [low, mid, high] = getLowestMiddleHighest(rgbVals);
+    if (low.val == 255) return;
+
+    let i = 1;
+    text.forEach((c) => {
+        const newSpan = document.createElement('span');
+        newSpan.innerHTML = c;
+
+        const lightRGB = [];
+        lightRGB[low.index] = Math.round(low.val + (Math.min(255 - low.val, i * 255)));
+        const incFraction = (lightRGB[low.index] - low.val) / (255 - low.val);
+        lightRGB[mid.index] = (mid.val + (255 - mid.val) * incFraction);
+        lightRGB[high.index] = (high.val + (255 - high.val) * incFraction);
+        newSpan.style.color = "#" + (lightRGB[0]).toString(16) + (lightRGB[1]).toString(16) + (lightRGB[2]).toString(16);
+
+        link.insertBefore(newSpan, current);
+        i -= (1 / text.length);
+    });
+
+    current.remove();
+
+} */
+
+/** Sorts RGB values from the given rgbIntArray in order of increasing magnitude.
+ * 
+ * Source: Hugh Haworth @ css-tricks.com
+ * https://css-tricks.com/using-javascript-to-adjust-saturation-and-brightness-of-rgb-colors/
+ * 
+ * @param {*} rgbIntArray 
+ * @returns the sorted rgb array
+ */
+/* function getLowestMiddleHighest(rgbIntArray) {
+    let highest = { val: -1, index: -1 };
+    let lowest = { val: Infinity, index: -1 };
+
+    rgbIntArray.map((val, index) => {
+        if (val > highest.val) {
+            highest = { val: val, index: index };
+        }
+        if (val < lowest.val) {
+            lowest = { val: val, index: index };
+        }
+    });
+    if (lowest.index === highest.index) {
+        lowest.index = highest.index + 1;
+    }
+    let middle = { index: (3 - highest.index - lowest.index) };
+    middle.val = rgbIntArray[middle.index];
+    console.log([lowest, middle, highest]);
+    return [lowest, middle, highest];
+} */
