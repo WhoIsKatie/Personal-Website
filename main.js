a0ad0ec3cd28e0a6c40397e0648959ea8ffe25f4
Katie Liu <katieliu505@gmail.com>
2022-12-31 00:53:39 -0500

    updated theme in remaining pages!! Todo: -implement js for featured project -change icon buttons :3

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+const navBarToggle = document.getElementById('js-navbar-toggle');
 const sections = document.getElementById('js-menu');
 const navbar = document.getElementById('navbar');
 const navLinks = document.querySelectorAll('li');
 var enable = true;
+
+navBarToggle.addEventListener('click', function () {
+    sections.classList.toggle('active');
+    navbar.classList.toggle('bgactive');
+    if (navbar.style.flexDirection != 'column')
+        navbar.style.flexDirection = 'column';
+    else navbar.style.flexDirection = 'row';
+
+    if (navbar.style.borderBottom != 'none')
+        navbar.style.borderBottom = 'none';
+    else
+        navbar.style.borderBottom = 'solid var(--thistle-border)';
+    enable = !enable;
+});
 
 navLinks.forEach((l) => {
     //gradientChars(l);
@@ -9,7 +24,6 @@ navLinks.forEach((l) => {
     l.addEventListener('click', () => {
         sections.classList.toggle('active');
         navbar.classList.toggle('bgactive');
-        //enable = !enable;
     })
 });
 
